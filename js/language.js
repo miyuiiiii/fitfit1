eebb81e750b2ee7aa83df143abebd730dc4e1242
Miyu Inoue <inoue54724@Aug1518-FVFV227VJ1WV.local>
2019-01-14 18:21:40 +0900

    Initial commit

---
+++ js/language.js
@@ -0,0 +1,20 @@
+var answers = ["C", "B", "A", "D"],
+  tot = answers.length;
+
+function getCheckedValue(radioName) {
+  var radios = document.getElementsByName(radioName); // Get radio group by-name
+  for (var y = 0; y < radios.length; y++)
+    if (radios[y].checked) return radios[y].value; // return the checked value
+}
+
+function getScore() {
+  var score = 0;
+  for (var i = 0; i < tot; i++)
+    if (getCheckedValue("question" + i) === answers[i]) score += 1; // increment only
+  return score;
+}
+
+function SubmitQuiz() {
+  document.getElementById("myresults").innerHTML =
+    "Your score is " + getScore() + "/" + tot;
+}
